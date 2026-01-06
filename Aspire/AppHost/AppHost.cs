1163c65c72313c9fcd24345e07887a48361a2a1d
İlhan Enes Daniş <danisenes3@gmail.com>
2026-01-06 21:27:24 +0300

    Add Keycloak integration and update project dependencies
    - Implemented Keycloak authentication in the Web API.
    - Added Keycloak parameters in the appsettings.json.
    - Updated AppHost to include Keycloak configuration.
    - Added necessary package references for Keycloak support.

--- Aspire/AppHost/AppHost.cs
+++ Aspire/AppHost/AppHost.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var userName = builder.AddParameter("KeycloakUserName");
+var password = builder.AddParameter("KeycloakPassword");
+
+var keycloak = builder.AddKeycloak("keycloak", 8080, userName, password)
+                      .WithDataVolume()
+                      .WithOtlpExporter();
+
+
+builder.AddProject<Projects.Web_API>("API");
+
 builder.Build().Run();
--- Src/Web.API/Program.cs
+++ Src/Web.API/Program.cs
@@ -2,6 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication().AddKeycloakJwtBearer("keycloak", realm: "Stocks", options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.Audience = "account";
+
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -10,6 +18,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/GetNumber", async () =>
+{
+    var number = Enumerable.Range(0, 1000);
+
+    return Results.Ok(number);
+}).RequireAuthorization();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
 
